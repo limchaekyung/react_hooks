f3daa822d0593f6ad20e61e259e548dc218a15bc
limchaekyung <corud505@naver.com>
2022-02-02 23:59:43 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React, {useState} from "react"
 import logo from './logo.svg';
 import './App.css';
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: {value}
+    } = event;
+    let WillUpdate = true;
+    if(typeof validator === "function"){
+      WillUpdate = validator(value);
+    }
+    if(WillUpdate){
+      setValue(value);
+    }
+  }
+  return {value, onChange};
+}
 const App = () =>{
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const maxLen = (value) => !value.includes("@");  //true return
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>IncremenetItem</button>
-      <button onClick={decrementItem}>decremenetItem</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" /* value={name.value} */ {...name}/>
     </div>
   );
 }
